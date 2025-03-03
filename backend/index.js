4fd853bd351fee375db930f006f5331410d055ee
brcOne <amv.0280@gmail.com>
2025-03-03 01:03:25 +0000

    stucture setup

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err)); 
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
